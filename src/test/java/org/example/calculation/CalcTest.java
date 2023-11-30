7a90670cc9787fcb161f6ba71d113a37c2bb076b
BrezinovaKaterina <144615454+BrezinovaKaterina@users.noreply.github.com>
2023-11-30 19:13:06 +0100

    Lekce10_DU_Testování
    Testy pro třídu Calc

--- src/test/java/org/example/calculation/CalcTest.java
+++ src/test/java/org/example/calculation/CalcTest.java
@@ -1,6 +1,7 @@
 package org.example.calculation;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class CalcTest {
@@ -11,4 +12,39 @@ public class CalcTest {
 
         Assertions.assertEquals(actual, expected);
     }
+
+    @Test
+    public void testDeleni(){
+        int actual = Calc.vydel(10,5);
+        int expected = 2;
+
+        Assertions.assertEquals(actual, expected);
+    }
+
+    @Test
+    @DisplayName("Násobení dvou čísel")
+    public void testNasobeni(){
+        int actual = Calc.vynasob(6,5);
+        int expected = 30;
+
+        Assertions.assertEquals(actual, expected);
+    }
+
+    @Test
+    @DisplayName("Dělení nulou")
+    public void testDeleniNulou(){
+        Exception exception = Assertions.assertThrows(ArithmeticException.class,() -> Calc.vydel(11,0));
+
+        Assertions.assertEquals("/ by zero", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("Test součtu čísel, která se nerovnají nule")
+    public void testSoucetVetsiNezNula(){
+        int actual = Calc.secti(10,15);
+        int expected = 0;
+
+        Assertions.assertTrue(actual > expected);
+    }
+
 }
