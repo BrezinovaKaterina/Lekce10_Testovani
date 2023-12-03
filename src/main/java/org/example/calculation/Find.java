3b35a685b7469f282f5ae509f003ceef168bb0b6
BrezinovaKaterina <144615454+BrezinovaKaterina@users.noreply.github.com>
2023-12-03 21:28:19 +0100

    Lekce10_DU_Testování
    Testy pro třídu Calc
    Testy pro třídu Find

--- src/main/java/org/example/calculation/Find.java
+++ src/main/java/org/example/calculation/Find.java
@@ -1,10 +1,20 @@
 package org.example.calculation;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Find {
 
     public static Double findMatching(List<Double> list, Double lowerLimit, Double upperLimit){
+        for (Double item : list){
+            if (item >= lowerLimit && item<=upperLimit){
+                return item;
+            }
+        }
+        return null;
+    }
+
+    public static Double findMatching2(List<Double> list, Double lowerLimit, Double upperLimit){
         for (Double item : list){
             if (item >= lowerLimit && item<=upperLimit){
                 return item;
--- src/test/java/org/example/calculation/FindTest.java
+++ src/test/java/org/example/calculation/FindTest.java
@@ -1,5 +1,41 @@
 package org.example.calculation;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 public class FindTest {
 
+    List<Double> testList;
+    @BeforeEach
+    public void setUp() {
+        testList = List.of(12.0, 8.126, 6.0);
+    }
+
+    @Test
+    @DisplayName("První vlastní test")
+    public void findMatching(){
+       Double testResult = Find.findMatching(testList, 11.0, 12.6);
+       Double expected = 12.0;
+
+        Assertions.assertEquals(expected, testResult);
+    }
+
+    List<Double> testList2;
+    @BeforeEach
+    public void setTestList2() {
+        testList2 = List.of(12.0, 8.126, 158.0, 9.0, 6.0);
+    }
+
+    @Test
+    @DisplayName("Druhý vlastní test")
+    public void findMatching2(){
+        Double testResult = Find.findMatching(testList2, 8.0, 10.0);
+        Double expected = 8.126;
+
+        Assertions.assertEquals(expected, testResult);
+    }
 }
